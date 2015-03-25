153226d758ab7c887f25cb870258341211f65b42
Vidya Venkiteswaran <vidya.vnv@gmail.com>
2015-03-25 18:53:46 +0530

    Change folder name to Predictions and Products

---
+++ Products/database/dbInfo.py
@@ -0,0 +1,7 @@
+"""
+This file includes the database,collection names to be used in the scripts.
+"""
+database = "snapdeal"
+productTable = "products"
+categoryTable = "categories"
+channelCategoryTable = "Channel_Category_Mapping"
