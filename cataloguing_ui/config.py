0bed09a0e5ab377f9e943b2ed013626afe54fd48
Ajay Singh <ajayk40@gmail.com>
2015-07-15 18:47:39 +0530

    remove config file
    Former-commit-id: 11266c84e20ef08c251a6dc68492ca8c6fb786d2

--- cataloguing_ui/config.py
+++
@@ -1,6 +0,0 @@
-#secret key
-APP_SECRET_KEY = '\x0e\xc9\x1f\xf9\xdf\n(\x96\xe0.T\xc8"\x81\x9c<'
-
-#FB credentials
-FB_CONSUMER_KEY = 366899470186425
-FB_CONSUMER_SECRET = 'fe508491f817f22ce59d2677512a220c'
