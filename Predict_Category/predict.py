34daf22fb451e714de49eb2e7983a674dd0f29cd
Shikhar Khattar <shikhar.khattar@delhivery.com>
2015-09-09 13:46:41 +0530

    spacing + alignment changed
    Former-commit-id: 408363d2f3709917b8ce8477f8d3aea4a8157a6a

--- Predict_Category/predict.py
+++ Predict_Category/predict.py
@@ -16,7 +16,7 @@ from logging.handlers import RotatingFileHandler
 PARENT_DIR_PATH=os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(PARENT_DIR_PATH)
 
-second_level_cat_names=\
+second_level_cat_names = \
             ["Beauty Products and Personal Care",
                   "Camera and Photos",
                   "Mobile Phone, Tablets and Accesories",
@@ -26,85 +26,87 @@ second_level_cat_names=\
                   "Home and Kitchen",
                   "Computers and Laptops",
                   "Shoes and Footwear"
-                 ]
-dangerous_cat_set=set()
-f_dang=open(PARENT_DIR_PATH+"/dangerous_categories.csv")
-reader=csv.DictReader(f_dang)
+            ]
+
+CLEAN_PRODUCT_NAME_REGEX = re.compile('[0-9.]+(?=[a-zA-Z]{1}[0-9]+)|[0-9.]+[a-zA-Z}{1}|[0-9.]+|[a-zA-Z]+')
+VOLUME_ML_REGEX = re.compile('[0-9]+[\s]*ml')
+
+dangerous_cat_set = set()
+f_dang = open(PARENT_DIR_PATH+"/dangerous_categories.csv")
+reader = csv.DictReader(f_dang)
 for row in reader:
-    if row['dang']=="1":
+    if row['dang'] == "1":
         dangerous_cat_set.add(row['cat_name'])
 f_dang.close()
-# print dang_set
 
-dangerous_word_set=set()
-f_dang=open(PARENT_DIR_PATH+"/dangerous_words.csv")
-reader=csv.reader(f_dang)
+dangerous_word_set = set()
+f_dang = open(PARENT_DIR_PATH + "/dangerous_words.csv")
+reader = csv.reader(f_dang)
 for row in reader:
     dangerous_word_set.add(row[0])
-#print dangerous_word_set
 f_dang.close()
 
-dangerous_ambi_set=set()
-f_dang=open(PARENT_DIR_PATH+"/dangerous_ambi.csv")
-reader=csv.reader(f_dang)
+dangerous_ambi_set = set()
+f_dang = open(PARENT_DIR_PATH + "/dangerous_ambi.csv")
+reader = csv.reader(f_dang)
 for row in reader:
     dangerous_ambi_set.add(row[0])
-#print dangerous_ambi_set
 f_dang.close()
 
-non_dangerous_set=set()
-f_dang=open(PARENT_DIR_PATH+"/non_dangerous_words.csv")
-reader=csv.reader(f_dang)
+non_dangerous_set = set()
+f_dang = open(PARENT_DIR_PATH + "/non_dangerous_words.csv")
+reader = csv.reader(f_dang)
 for row in reader:
     non_dangerous_set.add(row[0])
-#print non_dangerous_set
 f_dang.close()
 
-
-second_level_cat_names_set=set(second_level_cat_names)
+second_level_cat_names_set = set(second_level_cat_names)
 
 #logging file path
-LOGGING_PATH='/var/log/cat_subcat_logs/cat_subcat.log'
+LOGGING_PATH = '/var/log/cat_subcat_logs/cat_subcat.log'
 
 app = Flask(__name__)
-handler = RotatingFileHandler(LOGGING_PATH,maxBytes=10000000,backupCount=5)
+handler = RotatingFileHandler(LOGGING_PATH, maxBytes = 100000000, backupCount = 5)
 formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s")
 handler.setFormatter(formatter)
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
 
 app.logger.info("Loading Process Started")
-vectorizer=joblib.load(PARENT_DIR_PATH+'/Models/vectorizer.pkl')
-clf_bayes=joblib.load(PARENT_DIR_PATH+'/Models/clf_bayes.pkl')
-clf_chi=joblib.load(PARENT_DIR_PATH+'/Models/clf_chi.pkl')
-clf_rf=joblib.load(PARENT_DIR_PATH+'/Models/clf_l1_rf.pkl')
+vectorizer = joblib.load(PARENT_DIR_PATH + '/Models/vectorizer.pkl')
+clf_bayes = joblib.load(PARENT_DIR_PATH + '/Models/clf_bayes.pkl')
+clf_chi = joblib.load(PARENT_DIR_PATH + '/Models/clf_chi.pkl')
+clf_rf = joblib.load(PARENT_DIR_PATH + '/Models/clf_l1_rf.pkl')
 
-second_level_vectorizer={}
-second_level_clf_bayes={}
-second_level_clf_fpr={}
+second_level_vectorizer = {}
+second_level_clf_bayes = {}
+second_level_clf_fpr = {}
 for cat_name in second_level_cat_names_set:
-    second_level_vectorizer[cat_name]=joblib.load(PARENT_DIR_PATH+'/Models/SubModels/Vectorizer_'+cat_name)
-    second_level_clf_bayes[cat_name]=joblib.load(PARENT_DIR_PATH+'/Models/SubModels/clf_bayes_'+cat_name)
-    second_level_clf_fpr[cat_name]=joblib.load(PARENT_DIR_PATH+'/Models/SubModels/clf_fpr_'+cat_name)
+    second_level_vectorizer[cat_name] = joblib.load(PARENT_DIR_PATH +
+                                                    '/Models/SubModels/Vectorizer_' + cat_name)
+    second_level_clf_bayes[cat_name] = joblib.load(PARENT_DIR_PATH +
+                                                   '/Models/SubModels/clf_bayes_' + cat_name)
+    second_level_clf_fpr[cat_name] = joblib.load(PARENT_DIR_PATH +
+                                                 '/Models/SubModels/clf_fpr_' + cat_name)
 
 app.logger.info("Loading Process Complete")
 
 def predict_category(product_name):
-    app.logger.info("Request received {}".format(str(product_name)))
+    app.logger.info("Request received {}".format(product_name))
 
     try:
         l_product_name = product_name.lower()
-        product_words=re.findall("[0-9.]+(?=[a-zA-Z]{1}[0-9]+)|[0-9.]+[a-zA-Z}{1}|[0-9.]+|[a-zA-Z]+",l_product_name)
+        product_words = re.findall(CLEAN_PRODUCT_NAME_REGEX, l_product_name)
         clean_product_name = " ".join(product_words)
         class1 = clf_bayes.predict(vectorizer.transform([l_product_name]))[0]
         class2_prob_vector = clf_chi.predict_proba(vectorizer.transform([l_product_name]))[0]
         class3_prob_vector = clf_rf.predict_proba(vectorizer.transform([l_product_name]))[0]
 
-        if len(np.unique(class2_prob_vector))==1:
+        if len(np.unique(class2_prob_vector)) == 1:
             class2 = "Delhivery_Others"
         else:
             class2 = clf_bayes.classes_[np.argmax(class2_prob_vector)]
-        if len(np.unique(class3_prob_vector))==1:
+        if len(np.unique(class3_prob_vector)) == 1:
             class3 = "Delhivery_Others"
         else:
             class3 = clf_bayes.classes_[np.argmax(class3_prob_vector)]
@@ -126,37 +128,37 @@ def predict_category(product_name):
         if first_level in second_level_cat_names_set:
             prob_vector = second_level_clf_fpr[first_level].predict_proba(
                 second_level_vectorizer[first_level].transform([l_product_name]))[0]
-            if len(np.unique(prob_vector))==1:
+            if len(np.unique(prob_vector)) == 1:
                 second_level = second_level_clf_bayes[first_level].predict(
                     second_level_vectorizer[first_level].transform([l_product_name]))[0]
             else:
                 second_level = second_level_clf_bayes[first_level].classes_[np.argmax(prob_vector)]
 
-
         for word in non_dangerous_set:
             if word in clean_product_name:
-                clean_product_name=clean_product_name.replace(word," ")
+                clean_product_name = clean_product_name.replace(word, " ")
 
-        dangerous_flag=False
+        dangerous_flag = False
         for word in dangerous_word_set:
             if word in clean_product_name:
-                dangerous_flag=True
+                dangerous_flag = True
                 break
 
         if not dangerous_flag and first_level in dangerous_cat_set:
             for word in dangerous_ambi_set:
                 if word in clean_product_name:
-                    dangerous_flag=True
+                    dangerous_flag = True
                     break
-        if not dangerous_flag and re.search("[0-9]+[\s]*ml",clean_product_name):
-            dangerous_flag=True
-
+        # TODO: false positives in kitchenware etc ...
+        if not dangerous_flag and re.search(VOLUME_ML_REGEX, clean_product_name):
+            dangerous_flag = True
 
         # prob_vector= second_level_clf[class_name].predict_proba(
             #second_level_vectorizer[class_name].transform([product_name.lower()]))[0]
 
-        app.logger.info("Result produced {} --> {}".format(
-            str(first_level), str(second_level)))
+        app.logger.info("Result produced {} --> {}. Dangerous: {}".format(
+            first_level, second_level, dangerous_flag))
+    
     except Exception as err:
         app.logger.error(
             'Traceback: {}'.format(traceback.format_exc()))
@@ -172,20 +174,20 @@ def predict_category(product_name):
 def get_category():
     try:
         list_product_names = list(request.get_json())
-        output_list=[]
-        app.logger.info("Request Received {}".format (str(list_product_names)))
+        output_list = []
 
         for product_name_dict in list_product_names:
-            result = dict()
-            result['category'], result['sub_category'], result['dangerous'] = predict_category(product_name_dict.get('product_name',"").encode('ascii','ignore'))
+            result = {}
+            result['category'], result['sub_category'], result['dangerous'] = \
+                    predict_category(product_name_dict.get('product_name',"").encode('ascii','ignore'))
             output_list.append(result)
 
         return Response(json.dumps(output_list),  mimetype='application/json')
+    
     except Exception as err:
         app.logger.error(
             'Exception {} occurred against payload: {}'.format(
                 err, list_product_names))
-
 
 if __name__=='__main__':
     app.run()
@@ -207,3 +209,4 @@ if __name__=='__main__':
             first,second=predict_category(row[1])
             writer.writerow([row[0],row[1],first,second])
 """
+
