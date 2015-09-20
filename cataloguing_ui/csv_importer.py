fbc8ee18bba19d2885012c23b7f6e7181b496608
Ajay Singh <ajayk40@gmail.com>
2015-09-20 23:12:28 +0530

    bulk insertion
    Former-commit-id: b253ab82df753ecaada0fb7e7e870035ec98ce8f

--- cataloguing_ui/csv_importer.py
+++ cataloguing_ui/csv_importer.py
@@ -8,12 +8,15 @@ client = MongoClient()
 db = client.products_db
 
 data = []
+bulk = db.products.initialize_ordered_bulk_op()
 with open('data/HQSUBCAT.csv','rt') as f:
 	tempreader = csv.reader(f, delimiter=',')
 	next(tempreader, None)
 	for row in tempreader:
-		if len(row) > 2 and row[1] not in data:
+		if row[1] and row[2] and row[1] not in data:
 				data.append(row[1])
-				db.products.insert({'product_name':row[1], 'vendor':'HQSUBCAT',
+				bulk.insert({'product_name':row[1], 'vendor':'HQSUBCAT',
 									'category':row[2], 'sub_category': None, 'product_url': None})
+
+bulk.execute()
 print(len(data))
