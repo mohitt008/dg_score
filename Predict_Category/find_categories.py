88f8e0e763acf31d30f818e78b0d9ac7a2266783
Ajay Singh <ajayk40@gmail.com>
2015-09-23 15:33:25 +0530

    add objects and functions
    Former-commit-id: deced525d052b310297709374607016cf946414a

--- Predict_Category/find_categories.py
+++ Predict_Category/find_categories.py
@@ -8,80 +8,94 @@ import copy
 
 # SK: TODO: Integrate new dang logic and breakdown into functions
 def predict_category(product_name, cat_model, dang_model):
-    l_product_name = product_name.lower()
-    product_words = re.findall(CLEAN_PRODUCT_NAME_REGEX, l_product_name)
-    clean_product_name = " ".join(product_words)
-    
-    vectorizer = cat_model.vectorizer
-    clf_bayes = cat_model.clf_bayes
-    clf_chi = cat_model.clf_chi
-    clf_rf = cat_model.clf_rf
+    try:
+        l_product_name = product_name.lower()
+        product_words = re.findall(CLEAN_PRODUCT_NAME_REGEX, l_product_name)
+        clean_product_name = " ".join(product_words)
+        
+        vectorizer = cat_model.vectorizer
+        clf_bayes = cat_model.clf_bayes
+        clf_chi = cat_model.clf_chi
+        clf_rf = cat_model.clf_rf
 
-    second_level_vectorizer = cat_model.second_level_vectorizer
-    second_level_clf_bayes = cat_model.second_level_clf_bayes
-    second_level_clf_fpr = cat_model.second_level_clf_fpr
+        second_level_vectorizer = cat_model.second_level_vectorizer
+        second_level_clf_bayes = cat_model.second_level_clf_bayes
+        second_level_clf_fpr = cat_model.second_level_clf_fpr
 
-    class1 = clf_bayes.predict(vectorizer.transform([l_product_name]))[0]
-    class2_prob_vector = clf_chi.predict_proba(vectorizer.transform([l_product_name]))[0]
-    class3_prob_vector = clf_rf.predict_proba(vectorizer.transform([l_product_name]))[0]
+        class1 = clf_bayes.predict(vectorizer.transform([l_product_name]))[0]
+        class2_prob_vector = clf_chi.predict_proba(vectorizer.transform([l_product_name]))[0]
+        class3_prob_vector = clf_rf.predict_proba(vectorizer.transform([l_product_name]))[0]
 
-    if len(np.unique(class2_prob_vector)) == 1:
-        class2 = "Delhivery_Others"
-    else:
-        class2 = clf_bayes.classes_[np.argmax(class2_prob_vector)]
-    if len(np.unique(class3_prob_vector)) == 1:
-        class3 = "Delhivery_Others"
-    else:
-        class3 = clf_bayes.classes_[np.argmax(class3_prob_vector)]
-
-    if class3 == "Delhivery_Others":
-        if class1 == class2:
-            first_level = class1
-        elif class1 == "Delhivery_Others":
-            first_level = class2
-        elif class2 == "Delhivery_Others":
-            first_level = class1
+        if len(np.unique(class2_prob_vector)) == 1:
+            class2 = "Delhivery_Others"
         else:
-            first_level = class2
-    else:
-        first_level = class3
+            class2 = clf_bayes.classes_[np.argmax(class2_prob_vector)]
+        if len(np.unique(class3_prob_vector)) == 1:
+            class3 = "Delhivery_Others"
+        else:
+            class3 = clf_bayes.classes_[np.argmax(class3_prob_vector)]
 
-    second_level = ""
+        if class3 == "Delhivery_Others":
+            if class1 == class2:
+                first_level = class1
+            elif class1 == "Delhivery_Others":
+                first_level = class2
+            elif class2 == "Delhivery_Others":
+                first_level = class1
+            else:
+                first_level = class2
+        else:
+            first_level = class3
 
-    if first_level in cat_model.second_level_cat_names_set:
-        prob_vector = second_level_clf_fpr[first_level].predict_proba(
-            second_level_vectorizer[first_level].transform([l_product_name]))[0]
-        if len(np.unique(prob_vector)) == 1:
-            second_level = second_level_clf_bayes[first_level].predict(
+        second_level = ""
+
+        if first_level in cat_model.second_level_cat_names_set:
+            prob_vector = second_level_clf_fpr[first_level].predict_proba(
                 second_level_vectorizer[first_level].transform([l_product_name]))[0]
-        else:
-            second_level = second_level_clf_bayes[first_level].classes_[np.argmax(prob_vector)]
+            if len(np.unique(prob_vector)) == 1:
+                second_level = second_level_clf_bayes[first_level].predict(
+                    second_level_vectorizer[first_level].transform([l_product_name]))[0]
+            else:
+                second_level = second_level_clf_bayes[first_level].classes_[np.argmax(prob_vector)]
 
-    for word in dang_model.non_dangerous_set:
-        if word in clean_product_name:
-            clean_product_name = clean_product_name.replace(word, " ")
+        for word in dang_model.non_dangerous_set:
+            if word in clean_product_name:
+                clean_product_name = clean_product_name.replace(word, " ")
 
-    dangerous_flag = False
-    for word in dang_model.dangerous_word_set:
-        if word in clean_product_name:
-            dangerous_flag = True
-            break
-
-    if not dangerous_flag and first_level in dang_model.dangerous_cat_set:
-        for word in dang_model.dangerous_ambi_set:
+        dangerous_flag = False
+        for word in dang_model.dangerous_word_set:
             if word in clean_product_name:
                 dangerous_flag = True
                 break
-    
-    # TODO: false positives in kitchenware etc ...
-    if not dangerous_flag and re.search(VOLUME_ML_REGEX, clean_product_name):
-        dangerous_flag = True
 
-    result = {}
-    result['category'] = first_level
-    result['sub_category'] = second_level
-    result['dangerous'] = dangerous_flag
-    return result
+        if not dangerous_flag and first_level in dang_model.dangerous_cat_set:
+            for word in dang_model.dangerous_ambi_set:
+                if word in clean_product_name:
+                    dangerous_flag = True
+                    break
+        
+        # TODO: false positives in kitchenware etc ...
+        if not dangerous_flag and re.search(VOLUME_ML_REGEX, clean_product_name):
+            dangerous_flag = True
+
+        result = {}
+        result['category'] = first_level
+        result['sub_category'] = second_level
+        result['dangerous'] = dangerous_flag
+        return result
+
+    except Exception as err:
+        app.logger.error(
+            'Traceback: {}'.format(traceback.format_exc()))
+        app.logger.error(
+            'Exec Info: {}'.format(sys.exc_info())[0])
+        app.logger.error(
+            'Exception {} occurred against product: {}'.format(
+                err, product_name))
+        sentry_client.captureException(
+            message = "predict.py: Exception occured",
+            extra = {"error" : err, "product_name" : product_name})
+        
 
 # SK: TODO: Use log ?
 def process_product(product_name_dict, disque, cat_model, dang_model, log):
--- Predict_Category/predict.py
+++ Predict_Category/predict.py
@@ -7,49 +7,18 @@ import traceback
 import csv
 import re
 
+from flask import Flask, request,Response
+from logging.handlers import RotatingFileHandler
 from sklearn.externals import joblib
 
 from constants import second_level_cat_names, CLEAN_PRODUCT_NAME_REGEX, \
         VOLUME_ML_REGEX, ALPHA_NUM_REGEX, CACHE_EXPIRY
-
 from settings import r, sentry_client
+from find_categories import predict_category
 
-from flask import Flask, request,Response
-from logging.handlers import RotatingFileHandler
 
 PARENT_DIR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 sys.path.append(PARENT_DIR_PATH)
-
-dangerous_cat_set = set()
-f_dang = open(PARENT_DIR_PATH+"/dangerous_categories.csv")
-reader = csv.DictReader(f_dang)
-for row in reader:
-    if row['dang'] == "1":
-        dangerous_cat_set.add(row['cat_name'])
-f_dang.close()
-
-dangerous_word_set = set()
-f_dang = open(PARENT_DIR_PATH + "/dangerous_words.csv")
-reader = csv.reader(f_dang)
-for row in reader:
-    dangerous_word_set.add(row[0])
-f_dang.close()
-
-dangerous_ambi_set = set()
-f_dang = open(PARENT_DIR_PATH + "/dangerous_ambi.csv")
-reader = csv.reader(f_dang)
-for row in reader:
-    dangerous_ambi_set.add(row[0])
-f_dang.close()
-
-non_dangerous_set = set()
-f_dang = open(PARENT_DIR_PATH + "/non_dangerous_words.csv")
-reader = csv.reader(f_dang)
-for row in reader:
-    non_dangerous_set.add(row[0])
-f_dang.close()
-
-second_level_cat_names_set = set(second_level_cat_names)
 
 #logging file path
 LOGGING_PATH = '/var/log/cat_subcat_logs/cat_subcat.log'
@@ -62,106 +31,17 @@ app.logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
 
 app.logger.info("Loading Process Started")
-vectorizer = joblib.load(PARENT_DIR_PATH + '/Models/vectorizer.pkl')
-clf_bayes = joblib.load(PARENT_DIR_PATH + '/Models/clf_bayes.pkl')
-clf_chi = joblib.load(PARENT_DIR_PATH + '/Models/clf_chi.pkl')
-clf_rf = joblib.load(PARENT_DIR_PATH + '/Models/clf_l1_rf.pkl')
 
-second_level_vectorizer = {}
-second_level_clf_bayes = {}
-second_level_clf_fpr = {}
-for cat_name in second_level_cat_names_set:
-    second_level_vectorizer[cat_name] = joblib.load(PARENT_DIR_PATH +
-                                                    '/Models/SubModels/Vectorizer_' + cat_name)
-    second_level_clf_bayes[cat_name] = joblib.load(PARENT_DIR_PATH +
-                                                   '/Models/SubModels/clf_bayes_' + cat_name)
-    second_level_clf_fpr[cat_name] = joblib.load(PARENT_DIR_PATH +
-                                                 '/Models/SubModels/clf_fpr_' + cat_name)
+try:
+    dang_model = dangerousModel()
+    cat_model = categoryModel()
+except Exception as err:
+    logger.error("Error {} while loading models".format(err))
+    sentry_client.captureException(
+        message = "service_category_disque : Failed to load models",
+        extra = {"error" : err})
 
 app.logger.info("Loading Process Complete")
-
-def predict_category(product_name):
-
-    try:
-        l_product_name = product_name.lower()
-        product_words = re.findall(CLEAN_PRODUCT_NAME_REGEX, l_product_name)
-        clean_product_name = " ".join(product_words)
-        class1 = clf_bayes.predict(vectorizer.transform([l_product_name]))[0]
-        class2_prob_vector = clf_chi.predict_proba(vectorizer.transform([l_product_name]))[0]
-        class3_prob_vector = clf_rf.predict_proba(vectorizer.transform([l_product_name]))[0]
-
-        if len(np.unique(class2_prob_vector)) == 1:
-            class2 = "Delhivery_Others"
-        else:
-            class2 = clf_bayes.classes_[np.argmax(class2_prob_vector)]
-        if len(np.unique(class3_prob_vector)) == 1:
-            class3 = "Delhivery_Others"
-        else:
-            class3 = clf_bayes.classes_[np.argmax(class3_prob_vector)]
-
-        if class3 == "Delhivery_Others":
-            if class1 == class2:
-                first_level = class1
-            elif class1 == "Delhivery_Others":
-                first_level = class2
-            elif class2 == "Delhivery_Others":
-                first_level = class1
-            else:
-                first_level = class2
-        else:
-            first_level = class3
-
-        second_level = ""
-
-        if first_level in second_level_cat_names_set:
-            prob_vector = second_level_clf_fpr[first_level].predict_proba(
-                second_level_vectorizer[first_level].transform([l_product_name]))[0]
-            if len(np.unique(prob_vector)) == 1:
-                second_level = second_level_clf_bayes[first_level].predict(
-                    second_level_vectorizer[first_level].transform([l_product_name]))[0]
-            else:
-                second_level = second_level_clf_bayes[first_level].classes_[np.argmax(prob_vector)]
-
-        for word in non_dangerous_set:
-            if word in clean_product_name:
-                clean_product_name = clean_product_name.replace(word, " ")
-
-        dangerous_flag = False
-        for word in dangerous_word_set:
-            if word in clean_product_name:
-                dangerous_flag = True
-                break
-
-        if not dangerous_flag and first_level in dangerous_cat_set:
-            for word in dangerous_ambi_set:
-                if word in clean_product_name:
-                    dangerous_flag = True
-                    break
-        # TODO: false positives in kitchenware etc ...
-        if not dangerous_flag and re.search(VOLUME_ML_REGEX, clean_product_name):
-            dangerous_flag = True
-
-        # prob_vector= second_level_clf[class_name].predict_proba(
-
-        result = {}
-        result['category'] = first_level
-        result['sub_category'] = second_level
-        result['dangerous'] = dangerous_flag
-        return result
-
-        #second_level_vectorizer[class_name].transform([product_name.lower()]))[0]
-
-    except Exception as err:
-        app.logger.error(
-            'Traceback: {}'.format(traceback.format_exc()))
-        app.logger.error(
-            'Exec Info: {}'.format(sys.exc_info())[0])
-        app.logger.error(
-            'Exception {} occurred against product: {}'.format(
-                err, product_name))
-        sentry_client.captureException(
-            message = "predict.py: Exception occured",
-            extra = {"error" : err, "product_name" : product_name})
 
 @app.route('/get_category', methods = ['POST'])
 def get_category():
@@ -180,7 +60,7 @@ def get_category():
                 product_name_key = 'catfight:' +':' + product_name_clean
                 results_cache = r.get(product_name_key)
                 if not results_cache:
-                    results = predict_category(product_name.encode('ascii','ignore'))
+                    results = predict_category(product_name.encode('ascii','ignore'), cat_model, dang_model)
                     if results:
                         r.setex(product_name_key, json.dumps(results), CACHE_EXPIRY)
                         results['cached'] = False
@@ -224,7 +104,7 @@ if __name__=='__main__':
             firstrow = False
             continue
         if row[1]:
-            first,second=predict_category(row[1])
+            first,second=predict_category(row[1], cat_model, dang_model)
             writer.writerow([row[0],row[1],first,second])
 """
 
