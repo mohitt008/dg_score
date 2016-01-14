aa0a9559962a6d3e3a58640217d8fdebc0e5d155
Shikhar Khattar <shikhar.khattar@delhivery.com>
2016-01-14 18:39:37 +0530

    separated config
    Former-commit-id: 3d244d340bd37e4726da131996a4b627e93ee533
    Former-commit-id: 2fe63c92c9ce5cb86735f6f6efdfc5382bbf6885

---
+++ source/config/production.py
@@ -0,0 +1,9 @@
+# Sentry
+SENTRY_DSN = 'https://f65cb67d524443d2b8f9f53d3da0b3fd:5c6cb4e0d70d41efa9f77a585ade3dad@app.getsentry.com/48657'
+
+# Disque
+DISQUE_IP = '10.0.4.232:7711'
+
+# Redis
+REDIS_IP = 'data-prod-redis-002.iqi3ba.0001.apse1.cache.amazonaws.com'
+
