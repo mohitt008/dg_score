f248d17c5f11b10281d49f8644236dd9e6971ef7
rohan_anand <iitd.rohan@gmail.com>
2016-02-02 17:14:52 +0530

    Made local tests work
    Former-commit-id: 00a574f2f67771ec1625f903244ad47a72efeba8

--- source/Train_Model/utilities.py
+++ source/Train_Model/utilities.py
@@ -3,6 +3,7 @@ import csv
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from config.config_details import ROOT_PATH
+
 def get_color_set():
     x=set()
     f=open(ROOT_PATH +'/data/colors.csv')
@@ -11,4 +12,21 @@ def get_color_set():
         x.add(row[0].lower().strip())
     return x
 
+def get_category_tree():
+    category_tree={}
+    f=open(ROOT_PATH+'/data/category_tree.csv')
+    reader=csv.DictReader(f)
+    for row in reader:
+        if row.get('parent'):
+            if category_tree.get(row.get('parent')):
+                category_tree[row.get('parent')].append(row.get('category'))
+            else:
+                category_tree[row.get('parent')]=[row.get('category')]
+        else:
+            if not category_tree.get(row.get('category')):
+                category_tree[row.get('category')]=[]
+    return category_tree
+
+
 COLOR_SET=get_color_set()
+print get_category_tree()
