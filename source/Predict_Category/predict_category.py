ef0be2e04d565ff130fe4c947e3c7dbd4669a0db
Shikhar Khattar <shikhar.khattar@delhivery.com>
2016-02-08 13:34:02 +0530

    Removed unused predict.py Separated category prediction so that it can be used independently
    Former-commit-id: f790ce89b767ada5d0bad20de453024c35a2e6a5 [formerly 75ece4a1a4ffc80a8feb9f88e34c7ab1bf4e838b]
    Former-commit-id: 24c6219fcfd9d9012964526d808bd0aba59e3960

---
+++ source/Predict_Category/predict_category.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+# Product name should be passed after converting to lowercase
+def predict_category_tree (l_product_name, cat_model):
+    vectorizer = cat_model.vectorizer
+    clf_bayes = cat_model.clf_bayes
+    clf_chi = cat_model.clf_chi
+    clf_rf = cat_model.clf_rf
+
+    second_level_vectorizer = cat_model.second_level_vectorizer
+    second_level_clf_bayes = cat_model.second_level_clf_bayes
+    second_level_clf_fpr = cat_model.second_level_clf_fpr
+    second_level_clf_rf = cat_model.second_level_clf_rf
+
+    class1 = clf_bayes.predict(vectorizer.transform([l_product_name]))[0]
+    class2_prob_vector = clf_chi.predict_proba(vectorizer.transform([l_product_name]))[0]
+    class3_prob_vector = clf_rf.predict_proba(vectorizer.transform([l_product_name]))[0]
+
+    if len(np.unique(class2_prob_vector)) == 1:
+        class2 = "Delhivery_Others"
+    else:
+        class2 = clf_bayes.classes_[np.argmax(class2_prob_vector)]
+        if len(np.unique(class3_prob_vector)) == 1:
+            class3 = "Delhivery_Others"
+        else:
+            class3 = clf_bayes.classes_[np.argmax(class3_prob_vector)]
+
+    if class3 == "Delhivery_Others":
+        if class1 == class2:
+            first_level = class1
+        elif class1 == "Delhivery_Others":
+            first_level = class2
+        elif class2 == "Delhivery_Others":
+            first_level = class1
+        else:
+            first_level = class2
+    else:
+        first_level = class3
+
+    second_level = ""
+
+    if first_level in cat_model.second_level_cat_names_set_nb:
+        prob_vector = second_level_clf_fpr[first_level].predict_proba(
+            second_level_vectorizer[first_level].transform([l_product_name]))[0]
+        if len(np.unique(prob_vector)) == 1:
+            second_level = second_level_clf_bayes[first_level].predict(
+                second_level_vectorizer[first_level].transform([l_product_name]))[0]
+        else:
+            second_level = second_level_clf_bayes[first_level].classes_[np.argmax(prob_vector)]
+
+    elif first_level in cat_model.second_level_cat_names_set_rf:
+        prob_vector = second_level_clf_rf[first_level].predict_proba(
+            second_level_vectorizer[first_level].transform([l_product_name]))[0]
+        if len(np.unique(prob_vector)) == 1:
+            second_level = second_level_clf_bayes[first_level].predict(
+                second_level_vectorizer[first_level].transform([l_product_name]))[0]
+        else:
+            second_level = second_level_clf_bayes[first_level].classes_[np.argmax(prob_vector)]
+
+    return first_level, second_level
+
