5a81f21d8dbdfe0cd391327eff49c90e921deb24
Rahul Kumar <rahulkumar.iitd57@gmail.com>
2016-03-31 12:58:03 +0530

    Loading session in advance

--- source/Predict_Category/objects.py
+++ source/Predict_Category/objects.py
@@ -1,45 +1,96 @@
 import sys
 import csv
+import json
+import tensorflow as tf
 from sklearn.externals import joblib
 from constants import PARENT_DIR_PATH, MODELS_PATH, \
-        SUB_MODELS_PATH, DG_KEYWORDS_FILE
+    SUB_MODELS_PATH, DG_KEYWORDS_FILE
 
 sys.path.append(PARENT_DIR_PATH)
 
-from config.config_details import second_level_cat_names
+from Train_Model.text_cnn import TextCNN
+from config.config_details import second_level_cat_names, cnn_params
 
 
 class categoryModel(object):
+
     def __init__(self):
+        vocab_data = json.load(open(MODELS_PATH + "clf_cnn_vocab.txt"))
+        with tf.Graph().as_default():
+            session_conf = tf.ConfigProto(
+                allow_soft_placement=cnn_params['allow_soft_placement'],
+                log_device_placement=cnn_params['log_device_placement'])
+            sess = tf.Session(config=session_conf)
+            with sess.as_default():
+                cnn = TextCNN(
+                    sequence_length=vocab_data['sequence_length'],
+                    num_classes=len(vocab_data['vocabulary_inv_y']),
+                    vocab_size=len(vocab_data['vocabulary_x']),
+                    embedding_size=cnn_params['embedding_dim'],
+                    filter_sizes=cnn_params['filter_sizes'],
+                    num_filters=cnn_params['num_filters'],
+                    l2_reg_lambda=cnn_params['l2_reg_lambda'])
+                saver = tf.train.Saver()
+                saver.restore(sess, MODELS_PATH + "clf_cnn.ckpt")
+                self.clf_cnn = cnn
+                self.clf_cnn_sess = sess
+                self.clf_cnn_vocab_data = vocab_data
+
         self.second_level_cat_names_set = set(second_level_cat_names)
 
         self.vectorizer = joblib.load(MODELS_PATH + 'vectorizer.pkl',
-                                      mmap_mode = 'r')
+                                      mmap_mode='r')
         self.clf_bayes = joblib.load(MODELS_PATH + 'clf_bayes.pkl',
-                                     mmap_mode = 'r')
+                                     mmap_mode='r')
         self.clf_chi = joblib.load(MODELS_PATH + 'clf_chi.pkl',
-                                   mmap_mode = 'r')
+                                   mmap_mode='r')
         self.clf_fp = joblib.load(MODELS_PATH + 'clf_fp.pkl',
-                                  mmap_mode = 'r')
+                                  mmap_mode='r')
 
         self.second_level_vectorizer = {}
         self.second_level_clf_bayes = {}
         self.second_level_clf_fpr = {}
         self.second_level_clf_rf = {}
+        self.second_level_clf_cnn = {}
+        self.second_level_clf_cnn_sess = {}
+        self.second_level_clf_cnn_vocab_data = {}
 
         for cat_name in self.second_level_cat_names_set:
+            vocab_data = json.load(open(SUB_MODELS_PATH + "/clf_cnn_" +
+                                        cat_name.replace(' ', '_') + "_vocab.txt"))
+            with tf.Graph().as_default():
+                session_conf = tf.ConfigProto(
+                    allow_soft_placement=cnn_params['allow_soft_placement'],
+                    log_device_placement=cnn_params['log_device_placement'])
+                sess = tf.Session(config=session_conf)
+                with sess.as_default():
+                    cnn = TextCNN(
+                        sequence_length=vocab_data['sequence_length'],
+                        num_classes=len(vocab_data['vocabulary_inv_y']),
+                        vocab_size=len(vocab_data['vocabulary_x']),
+                        embedding_size=cnn_params['embedding_dim'],
+                        filter_sizes=cnn_params['filter_sizes'],
+                        num_filters=cnn_params['num_filters'],
+                        l2_reg_lambda=cnn_params['l2_reg_lambda'])
+                    saver = tf.train.Saver()
+                    saver.restore(sess, SUB_MODELS_PATH + "/clf_cnn_" + cat_name.replace(' ', '_'))
+                    self.second_level_clf_cnn[cat_name] = cnn
+                    self.second_level_clf_cnn_sess[cat_name] = sess
+                    self.second_level_clf_cnn_vocab_data[cat_name] = vocab_data
+
             self.second_level_vectorizer[cat_name] = joblib.load(
-                SUB_MODELS_PATH + '/Vectorizer_' + cat_name, mmap_mode = 'r')
+                SUB_MODELS_PATH + '/Vectorizer_' + cat_name, mmap_mode='r')
 
             self.second_level_clf_bayes[cat_name] = joblib.load(
-                SUB_MODELS_PATH + '/clf_bayes_' + cat_name, mmap_mode = 'r')
+                SUB_MODELS_PATH + '/clf_bayes_' + cat_name, mmap_mode='r')
 
             if cat_name in self.second_level_cat_names_set:
                 self.second_level_clf_fpr[cat_name] = joblib.load(
-                    SUB_MODELS_PATH + '/clf_fpr_' + cat_name, mmap_mode = 'r')
+                    SUB_MODELS_PATH + '/clf_fpr_' + cat_name, mmap_mode='r')
 
 
 class dangerousModel(object):
+
     def __init__(self):
         file_dg_csv = open(DG_KEYWORDS_FILE)
         reader = csv.reader(file_dg_csv)
--- source/Predict_Category/predict_category.py
+++ source/Predict_Category/predict_category.py
@@ -1,13 +1,10 @@
 import sys
-import json
 import numpy as np
-import tensorflow as tf
 from objects import categoryModel
-from constants import MODELS_PATH, SUB_MODELS_PATH, PARENT_DIR_PATH
+from constants import PARENT_DIR_PATH
 sys.path.append(PARENT_DIR_PATH)
 from config.config_details import cnn_params
 from Train_Model.data_utils import get_data_vector
-from Train_Model.text_cnn import TextCNN
 
 cat_model = categoryModel()
 
@@ -76,65 +73,38 @@ def predict_category_tree_using_cnn(l_product_name):
     """
     first_level = ""
     second_level = ""
-    vocab_data = json.load(open(MODELS_PATH + "clf_cnn_vocab.txt"))
+    vocab_data = cat_model.clf_cnn_vocab_data
     x = get_data_vector(l_product_name, vocab_data['vocabulary_x'], vocab_data['sequence_length'])
     y_rand = np.zeros((1, len(vocab_data['vocabulary_inv_y'])))
     y_rand[0][0] = 1
-    with tf.Graph().as_default():
-        session_conf = tf.ConfigProto(
-            allow_soft_placement=cnn_params['allow_soft_placement'],
-            log_device_placement=cnn_params['log_device_placement'])
-        sess = tf.Session(config=session_conf)
-        with sess.as_default():
-            cnn = TextCNN(
-                sequence_length=x.shape[1],
-                num_classes=len(vocab_data['vocabulary_inv_y']),
-                vocab_size=len(vocab_data['vocabulary_x']),
-                embedding_size=cnn_params['embedding_dim'],
-                filter_sizes=cnn_params['filter_sizes'],
-                num_filters=cnn_params['num_filters'],
-                l2_reg_lambda=cnn_params['l2_reg_lambda'])
-            saver = tf.train.Saver()
-            saver.restore(sess, MODELS_PATH + "clf_cnn.ckpt")
+    with cat_model.clf_cnn_sess.as_default():
+        cnn = cat_model.clf_cnn
+        feed_dict = {
+            cnn.input_x: x,
+            cnn.input_y: y_rand,
+            cnn.dropout_keep_prob: 1.0
+        }
+        scores, pred = cat_model.clf_cnn_sess.run([cnn.scores, cnn.predictions], feed_dict)
+        first_level = vocab_data['vocabulary_inv_y'][pred]
+        scores = scores[0]
+        first_level_scores = {category: scores[i]
+                              for i, category in enumerate(vocab_data['vocabulary_inv_y'])}
+        first_level_confidence_score = max(scores) / sum(scores)
+    if first_level in cat_model.second_level_cat_names_set:
+        with cat_model.second_level_clf_cnn_sess[first_level].as_default():
+            vocab_data = cat_model.second_level_clf_cnn_vocab_data[first_level]
+            x = get_data_vector(l_product_name, vocab_data[
+                                'vocabulary_x'], vocab_data['sequence_length'])
+            y_rand = np.zeros((1, len(vocab_data['vocabulary_inv_y'])))
+            y_rand[0][0] = 1
+            cnn = cat_model.second_level_clf_cnn[first_level]
             feed_dict = {
                 cnn.input_x: x,
                 cnn.input_y: y_rand,
                 cnn.dropout_keep_prob: 1.0
             }
-            scores, pred = sess.run([cnn.scores, cnn.predictions], feed_dict)
-            first_level = vocab_data['vocabulary_inv_y'][pred]
-            scores = scores[0]
-            first_level_scores = {category: scores[i]
-                                  for i, category in enumerate(vocab_data['vocabulary_inv_y'])}
-            first_level_confidence_score = max(scores) / sum(scores)
-    if first_level in cat_model.second_level_cat_names_set:
-        with tf.Graph().as_default():
-            session_conf = tf.ConfigProto(
-                allow_soft_placement=cnn_params['allow_soft_placement'],
-                log_device_placement=cnn_params['log_device_placement'])
-            sess = tf.Session(config=session_conf)
-            with sess.as_default():
-                vocab_data = json.load(open(SUB_MODELS_PATH + "/clf_cnn_" +
-                                            first_level.replace(' ', '_') + "_vocab.txt"))
-                x = get_data_vector(l_product_name, vocab_data[
-                                    'vocabulary_x'], vocab_data['sequence_length'])
-                y_rand = np.zeros((1, len(vocab_data['vocabulary_inv_y'])))
-                y_rand[0][0] = 1
-                cnn = TextCNN(
-                    sequence_length=x.shape[1],
-                    num_classes=len(vocab_data['vocabulary_inv_y']),
-                    vocab_size=len(vocab_data['vocabulary_x']),
-                    embedding_size=cnn_params['embedding_dim'],
-                    filter_sizes=cnn_params['filter_sizes'],
-                    num_filters=cnn_params['num_filters'],
-                    l2_reg_lambda=cnn_params['l2_reg_lambda'])
-                saver = tf.train.Saver()
-                saver.restore(sess, SUB_MODELS_PATH + "/clf_cnn_" + first_level.replace(' ', '_'))
-                feed_dict = {
-                    cnn.input_x: x,
-                    cnn.input_y: y_rand,
-                    cnn.dropout_keep_prob: 1.0
-                }
-                pred = sess.run([cnn.predictions], feed_dict)
-                second_level = vocab_data['vocabulary_inv_y'][pred[0]]
+            pred = cat_model.second_level_clf_cnn_sess[
+                first_level].run([cnn.predictions], feed_dict)
+            second_level = vocab_data['vocabulary_inv_y'][pred[0]]
+
     return first_level, second_level, first_level_scores, first_level_confidence_score
