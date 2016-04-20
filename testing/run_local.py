22946f58d7687c45b80696c4a1157e63206bc659
Shikhar Khattar <shikhar.khattar@delhivery.com>
2016-04-20 14:21:37 +0530

    remove multiprocessing

--- testing/run_local.py
+++ testing/run_local.py
@@ -1,8 +1,8 @@
 import sys
 import os.path
 import csv
-from multiprocessing import Pool, cpu_count
-from functools import partial
+# from multiprocessing import Pool, cpu_count
+# from functools import partial
 #import cProfile
 #import pstats
 #import StringIO
@@ -17,7 +17,7 @@ from Predict_Category.predict_category import *
 #output = []
 #cat_model = categoryModel()
 f2 = open("output.csv", 'w')
-l = ['prd', 'cat', 'subcat']
+l = ['prd', 'cat', 'subcat', 'cat_confidence']
 writer = csv.DictWriter(f2, fieldnames = l)
 writer.writeheader()
 
@@ -25,27 +25,19 @@ writer.writeheader()
 def collect_results(res, prd):
     #print "callback:", os.getpid()
     #print "callback:", res, type(res)
-    #print res
+    print res
     result = {}
     result['prd'] = prd
     result['cat'] = res[0]
     result['subcat'] = res[1]
+    result['cat_confidence'] = res[2]
     writer.writerow(result)
-
-
-"""
-def wtf(prd):
-    print os.getpid()
-    print "worker", prd.upper()
-    print "worker", type(cat_model)
-"""
-
 
 if __name__=="__main__":
     reader = csv.reader(open("input.csv"))
     # Ignore keys
     reader.next()
-    p = Pool(processes = cpu_count() - 2)
+    # p = Pool(processes = cpu_count() - 2)
     cnt = 0
 
     #pr = cProfile.Profile()
@@ -54,12 +46,18 @@ if __name__=="__main__":
     # SK: TODO: cache (Redis) ...
     for row in reader:
         try:
+            """
+            SK: Disabled multiprocessing (issues with tensor flow)
             prd = row[1].lower()
             custom_callback = partial(collect_results,
                                       prd = prd)
             p.apply_async(predict_category_tree,
                           args=(prd, ),
                           callback=custom_callback)
+            """
+            res = predict_category_tree_using_cnn(row[1])
+            collect_results(res, row[1])
+
             cnt += 1
             #print cnt
             if cnt > 50:
@@ -68,17 +66,8 @@ if __name__=="__main__":
             print e
             # continue
             break
-    p.close()
-    p.join()
+    #p.close()
+    #p.join()
 
     f2.close()
 
-"""
-    pr.disable()
-
-    s = StringIO.StringIO()
-    sortby = 'cumulative'
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    print s.getvalue()
-"""
